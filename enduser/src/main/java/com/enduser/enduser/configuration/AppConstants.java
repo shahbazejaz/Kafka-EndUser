80092202db19f4d6fc7c457b38eb25d525005676
Shahbazejaz <shahbazejaz098@gmail.com>
2024-12-11 13:53:50 +0530

    Initial files

---
+++ enduser/src/main/java/com/enduser/enduser/configuration/AppConstants.java
@@ -0,0 +1,8 @@
+package com.enduser.enduser.configuration;
+
+public class AppConstants {
+
+    public static final String LOCATION_UPDATE_TOPIC="location-update-topic";
+
+    public static final String GROUP_ID="group-1";
+}
